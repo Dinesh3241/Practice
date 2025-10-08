9e912070140b49967709a686280224d7f293ea3b
Dinesh_Reddy_Vulupala <148896096+Dinesh3241@users.noreply.github.com>
2025-10-08 22:56:07 +0530

    Exception Programs

--- Practice/src/Exception/finallyDemo.java
+++ Practice/src/Exception/finallyDemo.java
@@ -1,1 +1,20 @@
+package Exception;
+
+public class finallyDemo {
+
+	public static void main(String[] args) {
+		String s = null;
+		try {
+			System.out.println(s.length());
+		}catch(Exception e)
+		{
+			System.out.println("String has null value");
+		}
+		finally {
+			System.out.println("This will always executes...");
+		}
+
+	}
+
+}
 
