9a760517b2cb72ba2b7909626547607c701079ca
Dinesh_Reddy_Vulupala <148896096+Dinesh3241@users.noreply.github.com>
2025-10-08 23:00:25 +0530

    done

--- Practice/src/Array/Throws.java
+++
@@ -1,9 +0,0 @@
-public class Throws{
-    int i = 10;
-    int res = i/0;
-
-    public void Error() throws Exception{
-        System.out.println(res);
-        throw new Exception("Zero can't be in denominator");
-    } 
-}
--- Practice/src/Array/ThrowsMain.java
+++
@@ -1,7 +0,0 @@
-public class ThrowsMain {
-    public static void main(String[] args) throws Exception {
-        Throws t = new Throws();
-
-        t.Error();
-    }
-}
