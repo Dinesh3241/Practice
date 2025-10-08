5db8700b7815489d657feacb688194d3d72da935
Dinesh_Reddy_Vulupala <148896096+Dinesh3241@users.noreply.github.com>
2025-10-08 22:58:56 +0530

    ArrayList Programs

---
+++ Practice/src/Collections/ArrayList/ArrayListDemo.java
@@ -0,0 +1,20 @@
+package Collections;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> ar = new ArrayList<>();
+		
+		ar.add(3);
+		ar.add(4);
+		ar.add(5);
+		ar.remove(2);
+		
+		
+		System.out.print(ar);
+
+	}
+
+}
---
+++ Practice/src/Collections/ArrayList/ArrayListMain.java
@@ -0,0 +1,19 @@
+package Collections;
+
+import java.util.ArrayList;
+
+public class ArrayListMain {
+
+	public static void main(String[] args) {
+		ArrayList al = new ArrayList();
+		
+		al.add(3);
+		al.add("Dinesh");
+		al.add(null);
+		al.add(true);
+		
+		System.out.print(al);
+
+	}
+
+}
