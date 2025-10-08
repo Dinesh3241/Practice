50626078984f04882d75be52536c07c3ac827699
Dinesh_Reddy_Vulupala <148896096+Dinesh3241@users.noreply.github.com>
2025-10-08 14:59:30 +0530

    Encapsulation

---
+++ Practice/src/Encapsulation/EncapseMain.java
@@ -0,0 +1,9 @@
+public class EncapseMain {
+    public static void main(String[] args) {
+        encapsule e = new encapsule();
+
+        e.getId();
+        e.getName();
+        e.getRollNo();
+    }
+}
---
+++ Practice/src/Encapsulation/encapsule.java
@@ -0,0 +1,17 @@
+public class encapsule {
+    int id = 101;
+    String name = "Dinesh";
+    String RollNo = "22K91A6J9";
+
+    public void getId(){
+        System.out.println("Id : "+id);
+    }
+
+    public void getName(){
+        System.out.println("Name : "+name);
+    }
+
+    public void getRollNo(){
+        System.out.println("Roll No : "+RollNo);
+    }
+}
